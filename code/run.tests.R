1bbe97101f0b1af2b3fa064e80382d23ae3fc85f
akgoldberg <alexandergoldberg@college.harvard.edu>
2018-03-03 16:09:22 -0500

    tests 3 prep

--- code/run.tests.R
+++ code/run.tests.R
@@ -17,6 +17,7 @@ source('libraries.R')
 sigma.epsilon = NULL
 if (i==1) sigma.epsilon=diag(c(0.025, 0.01))
 if (i==2) sigma.epsilon=diag(c(0.025, 0.0005))
+if (i==3) sigma.epsilon = diag(0.025, 0.001, 0.0001)
 
 inference.tests <- run.inference.tests(i, 300, ~edges+gwesp(0.5, fixed=TRUE), dp.epsilon, sigma.epsilon=sigma.epsilon)
 save(inference.tests, file=sprintf("inference.tests%d-eps%g", i, dp.epsilon))
