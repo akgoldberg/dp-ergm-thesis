fc03e4b24c69213cc3e31eddbff6f501325eb9e2
akgoldberg <alexandergoldberg@college.harvard.edu>
2018-03-04 10:32:45 -0500

    printing method

--- code/run.tests.R
+++ code/run.tests.R
@@ -14,6 +14,8 @@ source('libraries.R')
 # df.samples <- run.noise.tests(samples, c(0.1, 0.5, 1.), dp.delta=1e-6, stop=1000) 
 # write.table(df.samples, file = sprintf("df.samples%d.txt",i), sep = ",", col.names = colnames(df.samples))
 
+print("Method is %s", method)
+
 if (i==1) {
     sigma.epsilon=diag(c(0.001, 0.0005))
     inference.tests <- run.inference.tests(i, 300, ~edges+gwesp(0.5, fixed=TRUE),
