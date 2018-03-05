07d1bcac3c9fc16e0ce39c7b64ab606d2e266dac
Alexander Goldberg <akgoldberg@rclogin09.rc.fas.harvard.edu>
2018-03-05 01:42:18 -0500

    updated tests

--- code/run.tests.R
+++ code/run.tests.R
@@ -19,19 +19,19 @@ if (i==1) {
     sigma.epsilon=diag(c(0.0005, 0.00025))
     inference.tests <- run.inference.tests(i, 300, ~edges+gwesp(0.5, fixed=TRUE),
                                             dp.epsilon, method=method, sigma.epsilon=sigma.epsilon,
-                                            non.private=FALSE)
+                                            non.private=FALSE, num.tests=25)
 }
 if (i==2) {
     sigma.epsilon=diag(c(0.0005, 0.00025))
     inference.tests <- run.inference.tests(i, 300, ~edges+gwesp(0.5, fixed=TRUE),
                                             dp.epsilon, method=method, sigma.epsilon=sigma.epsilon,
-                                            non.private=FALSE)
+                                            non.private=FALSE, num.tests=25)
 }
 if (i==3) {
     sigma.epsilon = diag(c(0.0005, 0.00025, 0.00001))
     inference.tests <- run.inference.tests(i, 300, ~edges+gwesp(0.5, fixed=TRUE)+gwdsp(0.5,fixed=TRUE),
                                             dp.epsilon, method=method, sigma.epsilon=sigma.epsilon,
-                                            non.private=FALSE)
+                                            non.private=FALSE, num.tests=25)
 } 
     
 save(inference.tests, file=sprintf("inference.tests%d%s-eps%g", i, method, dp.epsilon))
