9fcc53084a637bddab8bc45244cb219b11b38c63
akgoldberg <alexandergoldberg@college.harvard.edu>
2018-03-15 09:26:09 -0700

    fixed tests run

--- code/run.tests.inference.R
+++ code/run.tests.inference.R
@@ -3,8 +3,8 @@
       eval(parse(text=args[[k]]))
 }
 
-#setwd("/n/home06/akgoldberg/dp-ergm-thesis/code")
-setwd("/Users/alexandergoldberg/Documents/Harvard/Senior Year/Thesis/project/code")
+setwd("/n/home06/akgoldberg/dp-ergm-thesis/code")
+#setwd("/Users/alexandergoldberg/Documents/Harvard/Senior Year/Thesis/project/code")
 source('libraries.R')
 
 if (i==1) {
