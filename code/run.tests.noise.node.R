49b6b0e183a6ca84bf62c16aa74bd0000177cc74
akgoldberg <alexandergoldberg@college.harvard.edu>
2018-03-25 15:36:22 -0400

    node tests added

---
+++ code/run.tests.noise.node.R
@@ -0,0 +1,16 @@
+# get commands
+args <- (commandArgs(TRUE))
+for(k in 1:length(args)){
+  eval(parse(text=args[[k]]))
+}
+
+setwd("/n/home06/akgoldberg/dp-ergm-thesis/code")
+#setwd("/Users/alexandergoldberg/Documents/Harvard/Senior Year/Thesis/project/code")
+source('libraries.R')
+
+samples <- load.samples(i)
+df.samples <- run.noise.node.tests(samples, stop=1000) 
+write.table(df.samples, file = sprintf("df.samples%d_node.txt",i), sep = ",", col.names = colnames(df.samples))
+
+
+
